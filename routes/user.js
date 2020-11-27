3aa5808c434f82c30d6bc0e169ee0bfe8dcce7ab
sathyadev <shathi.ece@gmail.com>
2020-11-27 11:54:17 +0530

    Category product branch (#2)
    * added the code for user authentication
    
    * Added product and category API

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,13 @@ const express = require('express')
 
 const router = express.Router();
 
+const {	requireSignin,isAuth,isAdmin} = require("../controllers/auth");
 
+const {	userById} = require("../controllers/user");
+
+//TODO: isAdmin can be called for admin user ETA final course
+
+router.get('/secret/:userId', requireSignin, isAuth,isAdmin, (req,res) => {
 const {	requireSignin} = require("../controllers/auth");
 
 const {	userById} = require("../controllers/user");
@@ -12,7 +18,6 @@ router.get('/secret/:userId', requireSignin,(req,res) => {
 		user: req.profile
 	});
 });
-
 
 router.param('userId', userById)
 module.exports = router;
